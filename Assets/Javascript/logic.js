c5b17d85d405bc2469ee0a951f431c61e2e8112c
Francesca P <francescapuche@gmail.com>
2019-04-18 22:44:51 -0400

    JS add

--- Assets/Javascript/logic.js
+++ Assets/Javascript/logic.js
@@ -1,1 +1,18 @@
-console.log("Hi there"); 
+
+var topics = ["animals", "sports", "food"];
+
+function displayGifs () { 
+
+    var gifTopic = $(this).attr("data-topic"); 
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifTopic + "&api_key=2CvFpdItpqZcWorfO1phM1SeNygFzMZI&limit=7";
+    
+    $.ajax({ 
+        url: queryURL, 
+        method: "GET"
+    }).then(function(response) { 
+
+        
+
+    })
+}   
