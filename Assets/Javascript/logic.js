172aaa1c4cb1aa053b91aef76e3732db5d00b0de
Francesca P <francescapuche@gmail.com>
2019-06-20 12:42:25 -0400

    added for loop that only displays gifs pg-13 and under and adds div that holds the gifs

--- Assets/Javascript/logic.js
+++ Assets/Javascript/logic.js
@@ -14,12 +14,40 @@ function displayGifs () {
 
         console.log(response); 
 
-        var gifDiv = $("<div class='gif'>"); 
+        var results = response.data;
 
-        var rating = response.rating; 
-
-        var divOne = $("<div>").text("Rating: " + rating); 
-
+        for (var i =0; i < results.length; i++) {
+      
+             if (results[i].rating !== "pg-13" || results[i].rating !== "pg" || results[i].rating !== "g") {
+               //creating a div for the gif
+               var gifDiv = $("<div>");
+      
+               gifDiv.addClass("gifs");
+      
+               //Storing the result item's rating
+               var rating = results[i].rating;
+      
+               //creating a paragraph tag with the result item's rating
+               var p = $("<p>").text("Rating: " + rating);
+               p.addClass("rate");
+      
+               // Creating an image tag
+               var topicImage = $("<img>");
+      
+               //Giving the image tag an src attribute of a property pulled off the result item
+               topicImage.attr("src", results[i].images.fixed_height_still.url);
+               topicImage.attr("data-still", results[i].images.fixed_height_still.url);
+               topicImage.attr("data-animate", results[i].images.fixed_height.url);
+               topicImage.attr("data-state", "still");
+               topicImage.addClass('topicImage');
+      
+               gifDiv.append(p);
+               gifDiv.append(topicImage);
+      
+               //Prepending the gifDiv to the "#gifs-appear-here" in div in the HTML
+               $("#gifs-appear-here").prepend(gifDiv);
+          
+             }
         gifDiv.append(divOne); 
 
     })
