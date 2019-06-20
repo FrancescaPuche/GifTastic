2bb217b1bdef353d27ef39c2cbb9c675be766057
FrancescaPuche <30712733+FrancescaPuche@users.noreply.github.com>
2019-06-20 13:37:26 -0400

    added listen for elements with topic-btn

--- Assets/Javascript/logic.js
+++ Assets/Javascript/logic.js
@@ -1,91 +1,97 @@
+$(document).ready(function() {
+    var topics = ["animals", "sports", "food"];
 
-var topics = ["animals", "sports", "food"];
+    function displayGifs () { 
 
-function displayGifs () { 
+        var gifTopic = $(this).attr("data-topic"); 
 
-    var gifTopic = $(this).attr("data-topic"); 
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifTopic + "&api_key=2CvFpdItpqZcWorfO1phM1SeNygFzMZI&limit=7";
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifTopic + "&api_key=2CvFpdItpqZcWorfO1phM1SeNygFzMZI&limit=7";
-    
-    $.ajax({ 
-        url: queryURL, 
-        method: "GET"
-    }).then(function(response) { 
+        $.ajax({ 
+            url: queryURL, 
+            method: "GET"
+        }).then(function(response) { 
 
-        console.log(response); 
+            console.log(response); 
 
-        var results = response.data;
+            var results = response.data;
 
-        for (var i =0; i < results.length; i++) {
-      
-            if (results[i].rating !== "pg-13" || results[i].rating !== "pg" || results[i].rating !== "g") {
-               
-                var gifDiv = $("<div>");
-      
-                gifDiv.addClass("gifs");
-      
-                var rating = results[i].rating;
-      
-                var p = $("<p>").text("Rating: " + rating);
-                p.addClass("rate");
-      
-                var topicImage = $("<img>");
-      
-                topicImage.attr("src", results[i].images.fixed_height_still.url);
-                topicImage.attr("data-still", results[i].images.fixed_height_still.url);
-                topicImage.attr("data-animate", results[i].images.fixed_height.url);
-                topicImage.attr("data-state", "still");
-                topicImage.addClass('topicImage');
-      
-                gifDiv.append(p);
-                gifDiv.append(topicImage);
-      
-                $("#gifs-appear-here").prepend(gifDiv);
-            }
+            for (var i =0; i < results.length; i++) {
 
-            $(".topicImage").on("click", function() { 
-                var state = $(this).attr("data-state"); 
-                console.log(state); 
+                if (results[i].rating !== "pg-13" || results[i].rating !== "pg" || results[i].rating !== "g") {
 
-                if(state === "still") { 
-                    $(this).attr("src", $(this).attr("data-animare")); 
-                    $(this).attr("data-state", "animate"); 
-                } 
-                else { 
-                    $(this).attr("src", $(this).attr("data-still")); 
-                    $(this).attr("data-state", "still"); 
+                    var gifDiv = $("<div>");
+
+                    gifDiv.addClass("gifs");
+
+                    var rating = results[i].rating;
+
+                    var p = $("<p>").text("Rating: " + rating);
+                    p.addClass("rate");
+
+                    var topicImage = $("<img>");
+
+                    topicImage.attr("src", results[i].images.fixed_height_still.url);
+                    topicImage.attr("data-still", results[i].images.fixed_height_still.url);
+                    topicImage.attr("data-animate", results[i].images.fixed_height.url);
+                    topicImage.attr("data-state", "still");
+                    topicImage.addClass('topicImage');
+
+                    gifDiv.append(p);
+                    gifDiv.append(topicImage);
+
+                    $("#gifs-appear-here").prepend(gifDiv);
                 }
-            });
-        }
-    });
-}
 
-function buttons () { 
+                $(".topicImage").on("click", function() { 
+                    var state = $(this).attr("data-state"); 
+                    console.log(state); 
 
-    // for deleting movies 
-    $("#buttons").empty();
-
-    for (var i = 0; i < topics.length; i++) { 
-
-        var btn = $("<button>"); 
-
-        btn.addClass("topic-btn btn btn-danger"); 
-        btn.attr("data-topic", topics[i]); 
-        btn.text(topics[i]); 
-
-        $("#buttons").append(btn); 
+                    if(state === "still") { 
+                        $(this).attr("src", $(this).attr("data-animare")); 
+                        $(this).attr("data-state", "animate"); 
+                    } 
+                    else { 
+                        $(this).attr("src", $(this).attr("data-still")); 
+                        $(this).attr("data-state", "still"); 
+                    }
+                });
+            }
+        });
     }
-}
 
-$("#add-gif").on("click", function(event) { 
+    function buttons () { 
 
-    event.preventDefault(); 
+        // for deleting movies 
+        $("#buttons").empty();
 
-    var topicGif = $("#gif-input").val().trim(); 
+        for (var i = 0; i < topics.length; i++) { 
 
-    topics.push(topicGif); 
+            var btn = $("<button>"); 
 
-    buttons();
+            btn.addClass("topic-btn btn btn-danger"); 
+            btn.attr("data-topic", topics[i]); 
+            btn.text(topics[i]); 
+
+            $("#buttons").append(btn); 
+        }
+    }
+
+    $("#add-gif").on("click", function(event) { 
+
+        event.preventDefault(); 
+
+        var topicGif = $("#gif-input").val().trim(); 
+
+        topics.push(topicGif); 
+
+        buttons();
+    });
+
+        $(document).on("click", ".topic-btn", displayGifs); 
+
+        buttons(); 
+
 });
 
 
